8e728f6d6f1bcb00470b9a841b9826d5e4e03f13
JoelyPolancox3 <chadcharela@gmail.com>
2026-08-21 00:57:56 -0600

    Create datos.py with client list
    Added a list of beauty salon clients with their details.

---
+++ datos.py
@@ -0,0 +1,27 @@
+datos.py
+# Lista de clientes del salón de belleza
+
+clientes = [
+    {
+        "id": "1",
+        "nombre": "Ana",
+        "servicio": "Corte",
+        "precio": 250,
+        "visitas": 3
+    },
+    {
+
+        "id": "2",
+        "nombre": "Estyven",
+        "servicio": "Tinte",
+        "precio": 500,
+        "visitas": 2
+    },
+    {
+        "id": "3",
+        "nombre": "Joely",
+        "servicio": "Manicura",
+        "precio": 150,
+        "visitas": 5
+    }
+]
