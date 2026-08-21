9d05d02cfe68af8ac585cf9d04c94ae7167262d3
JoelyPolancox3 <chadcharela@gmail.com>
2026-08-21 00:30:30 -0600

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from datos import inventario
-from funciones import sumar_valores_diccionario
-
-total= sumar_valores_diccionario(inventario)
-print(f"La cantidad total de items disponibles en el inventario es: {total}")
